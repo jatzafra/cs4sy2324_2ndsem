c1f4b4d5c32d14987b635ca0a350a3fc22101e1c
TRUTH <TRUTH@172.22.16.122>
2024-02-13 14:27:14 +0800

    new project repository

---
+++ src/exercise12truthzafrajulian/main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise12truthzafrajulian;
+
+
+/**
+ *
+ * @author TRUTH
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Subject math = new Subject("Math", "math.png", 4, 1.75);
+        Subject bio = new Subject("Biology", "biology.png", 3, 2.0);
+        Subject chem = new Subject("Chemistry", "chemistry.png", 3, 1.5);
+        Subject physics = new Subject("Physics", "physics.png", 3, 1.75);
+        Subject cs = new Subject("CS", "computer science.png", 1, 1.5);
+        
+        Subject displayedSubject = cs;
+        
+        
+    }
+    
+}
